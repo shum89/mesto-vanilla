cf5ca18a77762f5865de25e591188a4b0b378a43
Filipp Shumakov <shum8929@gmail.com>
2020-04-11 14:54:34 +0300

    hotfix: небольшие правки в стилях и скрипте

---
+++ script/script.js
@@ -0,0 +1,44 @@
+const popup = document.querySelector('.popup');
+const popupContainer = popup.querySelector('.popup__container');
+const popupForm = popupContainer.querySelector('.popup__form');
+let popupInputTitle = popupContainer.querySelector('.popup__input-title');
+let popupInputSubtitle = popupContainer.querySelector('.popup__input-subtitle');
+const buttonSubmit = popupContainer.querySelector('.popup__button-submit');
+const buttonClose = popupContainer.querySelector('.popup__button-close');
+
+const profile = document.querySelector('.profile');
+let profileTitle = profile.querySelector('.profile__title');
+let profileSubtitle = profile.querySelector('.profile__subtitle');
+const editButton = profile.querySelector('.profile__edit-button');
+
+
+// открывающая и закрывающая popup функиця
+function openOrClosePopup() {
+    // проверяем есть ли класс скрывающий popup
+    if (popup.classList.contains('popup_hidden')) {
+    popup.classList.toggle('popup_hidden');
+    popup.classList.add('popup_opened');
+    popupInputTitle.value = profileTitle.textContent;
+    popupInputSubtitle.value = profileSubtitle.textContent;
+    } else {
+        popup.classList.toggle('popup_hidden');
+        popup.classList.toggle('popup_opened');
+    }
+};
+
+// отправляем изменения в profile
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    profileTitle.textContent = popupInputTitle.value;
+    profileSubtitle.textContent = popupInputSubtitle.value;
+    openOrClosePopup();
+}
+
+
+popupForm.addEventListener('submit',formSubmitHandler);
+
+editButton.addEventListener('click', openOrClosePopup);
+
+buttonClose.addEventListener('click', openOrClosePopup);
+
